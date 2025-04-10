e2b0aafc709ba12f3dfb381e1df0969efaba4df8
Jose Delgado <j3leonardodelgado@gmail.com>
2025-04-10 02:28:03 -0400

    fix: update layout body class for full-width display

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased bg-blue-950 text-white h-screen`}
+                className={`${geistSans.variable} ${geistMono.variable} antialiased bg-blue-950 text-white h-screen w-full`}
             >
                 <Header />
                 <main>{children}</main>
