8ca842d8642dcb5ac09e882ee0a9c520448c91fa
Jose Delgado <j3leonardodelgado@gmail.com>
2025-04-11 01:47:25 -0400

    fix: refactor removeDuplicates function for improved readability and structure

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Anime } from '@/types/anime';
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+export function removeDuplicates(array: Anime[]) {
+    const uniqueSet = new Set();
+
+    return array.filter((obj) => {
+        const objString = JSON.stringify(obj);
+        if (!uniqueSet.has(objString)) {
+            uniqueSet.add(objString);
+            return true;
+        }
+        return false;
+    });
 }
