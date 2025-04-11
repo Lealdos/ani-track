0af3c54cd1004ccce5cc078e260e78281049eb57
Jose Delgado <j3leonardodelgado@gmail.com>
2025-04-11 01:07:34 -0400

    fix: simplify structure of AnimeList component for better readability

--- src/components/anime-list.tsx
+++ src/components/anime-list.tsx
@@ -9,21 +9,15 @@ interface AnimeListProps {
 export function AnimeList({ animes, showBadge = false }: AnimeListProps) {
     if (!animes) return null;
     return (
-        <div className='relative'>
-            <div
-                id='anime-scroll-container'
-                className='flex gap-4 overflow-x-auto pb-4 scrollbar-hide md:flex-wrap justify-between '
-                style={{ scrollBehavior: 'smooth' }}
-            >
-                {animes.map((anime) => (
-                    <div
-                        key={`${anime.title}-${anime.mal_id}`}
-                        className='w-[160px] flex-shrink-0 sm:w-[180px]'
-                    >
-                        <AnimeCard anime={anime} showBadge={showBadge} />
-                    </div>
-                ))}
-            </div>
+        <div className='flex gap-4 overflow-x-auto pb-4 scrollbar-hide md:flex-wrap justify-between md:grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6  border-b-2 border-b-background/10 justify-items-center-safe  rounded-md'>
+            {animes.map((anime) => (
+                <div
+                    key={`${anime.title}-${anime.mal_id}`}
+                    className='w-[160px] flex-shrink-0 sm:w-[180px]'
+                >
+                    <AnimeCard anime={anime} showBadge={showBadge} />
+                </div>
+            ))}
         </div>
     );
 }
