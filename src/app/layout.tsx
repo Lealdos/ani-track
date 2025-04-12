d9aa868d11a5e054a32c1de3c35de6b3490af49f
Jose Delgado <j3leonardodelgado@gmail.com>
2025-04-11 20:37:17 -0400

    fix: update background color in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased bg-blue-950 text-white h-dvh w-full`}
+                className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-950 text-white h-dvh w-full`}
             >
                 <Header />
                 {children}
