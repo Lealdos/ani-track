9ccee85b4f4ea9b36b6fc8ee9e35b6519dfdf3d8
lealdo-laptop <j3leonardodelgado@gmail.com>
2025-04-13 21:26:00 -0400

    feat: comment out AnimeListSkeleton and adjust grid gap for improved layout consistency in the current season display

--- src/components/anime-list.tsx
+++ src/components/anime-list.tsx
@@ -14,7 +14,7 @@ export function AnimeList({
 }: AnimeListProps) {
     if (!animes) return null
     return (
-        <main className="scrollbar-hide flex snap-x gap-4 overflow-x-auto px-4 pb-4 md:grid md:grid-cols-3 md:gap-8 md:overflow-visible lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5">
+        <main className="scrollbar-hide flex snap-x gap-4 overflow-x-auto px-4 pb-4 md:grid md:grid-cols-3 md:gap-4 md:overflow-visible lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5">
             {animes.map((animeItem) => (
                 <div
                     key={`${animeItem.title}-${animeItem.mal_id}-${SectionName}`}
