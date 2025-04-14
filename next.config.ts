39438e81d5938ea8756e8c0c931d71835106a9ce
lealdo-laptop <j3leonardodelgado@gmail.com>
2025-04-13 21:26:23 -0400

    feat: refine image URL path in next.config.ts to target specific anime images for better resource management

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
                 protocol: 'https',
                 hostname: 'cdn.myanimelist.net',
                 port: '',
-                pathname: '/**/**',
+                pathname: '/images/anime/**/**',
                 search: '',
             },
         ],
