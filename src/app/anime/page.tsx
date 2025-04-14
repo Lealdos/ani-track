156c0f9b1e512a8812e8581897b75d7566eb7b68
lealdo-laptop <j3leonardodelgado@gmail.com>
2025-04-14 17:55:39 -0400

    feat: enhance BrowseAnime component with loading state and not found handling for improved user experience in page.tsx

--- src/app/anime/page.tsx
+++ src/app/anime/page.tsx
@@ -1,11 +1,19 @@
 import { getAllAnimes } from '@/lib/api'
 import { AnimeList } from '@/components/anime-list'
+import { notFound } from 'next/navigation'
+import { Suspense } from 'react'
+import { AnimeListSkeleton } from '@/components/ui/SkeletonCard/AnimeSkeletonList'
 
 export default async function BrowseAnime() {
     const { data: anime, pagination } = await getAllAnimes(2)
+    if (!anime) {
+        return notFound()
+    }
     return (
         <div className="container mx-auto min-h-screen w-full px-4 py-8 text-white">
-            <AnimeList animes={anime} showBadge />
+            <Suspense fallback={<AnimeListSkeleton />}>
+                <AnimeList animes={anime} showBadge />
+            </Suspense>
             <div className="mt-4 flex flex-row items-center justify-between">
                 <button className="rounded-lg bg-primary px-4 py-2 text-sm font-medium text-primary-foreground hover:bg-primary/90">
                     {pagination.current_page > 1 ? '←' : ''} Previous page
