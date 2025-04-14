08353cd15b611e90b07e9676a39d5a63b5ffe7b2
lealdo-laptop <j3leonardodelgado@gmail.com>
2025-04-13 21:27:03 -0400

    feat: adjust layout in AnimeListSkeleton by refining grid gap for better consistency in displaying anime items

--- src/components/ui/SkeletonCard/AnimeSkeletonList.tsx
+++ src/components/ui/SkeletonCard/AnimeSkeletonList.tsx
@@ -10,7 +10,7 @@ export function AnimeListSkeleton({
     skeletonItemCount = 12,
 }: AnimeListSkeletonProps) {
     return (
-        <section className="scrollbar-hide snap-x gap-4 overflow-x-auto px-4 pb-4 md:grid md:grid-cols-3 md:gap-8 md:overflow-visible lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5">
+        <section className="scrollbar-hide md:gap- snap-x gap-4 overflow-x-auto px-4 pb-4 md:grid md:grid-cols-3 md:overflow-visible lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5">
             {[...Array(skeletonItemCount)].map((_, i) => (
                 <SkeletonCard key={`${sectionName}-${i}`} />
             ))}
