eae2cae8c4358cba2d61b09429c1ab4d04cfe377
Jose Delgado <j3leonardodelgado@gmail.com>
2025-04-15 17:12:01 -0400

    fix: simplify remotePatterns configuration for image loading in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'cdn.myanimelist.net',
-                port: '',
-                pathname: '/images/anime/**/**',
-            },
+            new URL('https://cdn.myanimelist.net/images/anime/**'),
         ],
     },
 }
