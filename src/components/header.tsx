525e7eeded7bf9e971941cb2913a798fa784a75a
lealdo-laptop <j3leonardodelgado@gmail.com>
2025-04-20 17:51:16 -0400

    fix: improve mobile menu functionality and styling in header component for better user experience and usability

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,25 +5,44 @@
 
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-import { Menu, X } from 'lucide-react'
+import { Menu, X, User } from 'lucide-react'
 import { SearchBar } from './search-bar'
 import { cn } from '@/lib/utils'
 
 export default function Header() {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [isScrolled, setIsScrolled] = useState(false)
     // const pathname = usePathname()
     // const { user, signOut } = useAuth()
-
-    const [isScrolled, setIsScrolled] = useState(false)
 
     useEffect(() => {
         const handleScroll = () => {
             setIsScrolled(window.scrollY > 10)
         }
 
+        const handleClickOutside = (event: MouseEvent) => {
+            const drawer = document.getElementById('mobile-drawer')
+            const menuButton = document.getElementById('menu-button')
+
+            if (
+                isMenuOpen &&
+                drawer &&
+                !drawer.contains(event.target as Node) &&
+                menuButton &&
+                !menuButton.contains(event.target as Node)
+            ) {
+                setIsMenuOpen(false)
+            }
+        }
+
         window.addEventListener('scroll', handleScroll)
-        return () => window.removeEventListener('scroll', handleScroll)
-    }, [])
+        document.addEventListener('mousedown', handleClickOutside)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [isMenuOpen])
 
     const routes = [{ href: '/anime', label: 'Browse' }]
 
@@ -32,14 +51,14 @@ export default function Header() {
             className={cn(
                 `sticky top-0 z-50 flex w-full border-b`,
                 isScrolled
-                    ? 'bg-gradient-to-r from-slate-900/90 via-purple-900/85 to-slate-900/90 shadow-md backdrop-blur-sm'
+                    ? 'bg-gradient-to-r from-slate-900/85 via-purple-900/80 to-slate-900/85 shadow-md backdrop-blur-sm'
                     : 'bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900 backdrop-blur'
             )}
         >
             <div className="mx-4 flex h-18 grow-4 items-center justify-center gap-8 md:mx-8">
                 <div className="flex">
                     <Link href="/" className="flex items-center">
-                        <span className="bg-gradient-to-bl from-red-500 to-red-800 bg-clip-text text-xl font-bold text-transparent">
+                        <span className="bg-gradient-to-bl from-red-500 to-red-800 bg-clip-text font-sans text-xl font-bold text-transparent italic">
                             AniTrack
                         </span>
                     </Link>
@@ -72,53 +91,70 @@ export default function Header() {
                             <button className="text-sm">Sign up</button>
                         </Link>
                     </div> */}
-
-                    <button
-                        className="flex items-center justify-center rounded-md p-2 md:hidden"
-                        onClick={() => setIsMenuOpen(!isMenuOpen)}
-                    >
-                        {isMenuOpen ? (
-                            <X className="h-6 w-6" />
-                        ) : (
-                            <Menu className="h-6 w-6" />
-                        )}
-                    </button>
                 </div>
             </div>
 
             {/* Mobile menu */}
+
+            <button
+                id="menu-button"
+                className="relative flex items-center justify-center rounded-md p-2 md:hidden"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+                {!isMenuOpen && <Menu className="h-6 w-6" strokeWidth={2} />}
+            </button>
             {isMenuOpen && (
-                <div className="container pb-4 md:hidden">
-                    <nav className="mx-4 flex flex-col items-end space-y-4">
-                        {routes.map((route) => (
+                <div
+                    id="mobile-drawer"
+                    className={`fixed top-0 right-0 z-40 container h-screen w-72 -translate-x-full overflow-y-auto bg-gradient-to-tr from-slate-900/90 via-purple-900/90 to-slate-900/90 p-4 shadow-md backdrop-blur-sm transition-transform ${
+                        isMenuOpen ? 'translate-x-0' : '-translate-x-full'
+                    }`}
+                    tabIndex={-1}
+                    aria-labelledby="drawer-right-label"
+                >
+                    <h5
+                        id="drawer-right-label"
+                        className="mb-4 inline-flex items-center text-base font-semibold text-white"
+                    >
+                        <User className="mr-2.5 h-4 w-4" />
+                        Menu
+                    </h5>
+                    <button
+                        type="button"
+                        onClick={() => setIsMenuOpen(false)}
+                        className="absolute end-2.5 top-2.5 inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-white"
+                    >
+                        <X className="h-6 w-6" />
+                        <span className="sr-only">Close menu</span>
+                    </button>
+                    <ul className="space-y-2 text-white">
+                        <li>
                             <Link
-                                key={route.href}
-                                href={route.href}
-                                className={`text-white transition-colors hover:scale-105 hover:text-cyan-500`}
-                                onClick={() => setIsMenuOpen(false)}
+                                href="/login"
+                                className="flex items-center rounded-lg p-2 hover:bg-gray-700 hover:text-white"
                             >
-                                {route.label}
+                                <span>Login</span>
                             </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/register"
+                                className="flex items-center rounded-lg p-2 hover:bg-gray-700 hover:text-white"
+                            >
+                                <span>Register</span>
+                            </Link>
+                        </li>
+                        {routes.map((route) => (
+                            <li key={route.href}>
+                                <Link
+                                    href={route.href}
+                                    className="flex items-center rounded-lg p-2 hover:bg-gray-700 hover:text-white"
+                                >
+                                    <span>{route.label}</span>
+                                </Link>
+                            </li>
                         ))}
-                        {/* {!user && (
-              <>
-                <Link
-                  href="/login"
-                  className="text-sm font-medium transition-colors"
-                  onClick={() => setIsMenuOpen(false)}
-                >
-                  Log in
-                </Link>
-                <Link
-                  href="/register"
-                  className="text-sm font-medium transition-colors"
-                  onClick={() => setIsMenuOpen(false)}
-                >
-                  Sign up
-                </Link>
-              </>
-            )} */}
-                    </nav>
+                    </ul>
                 </div>
             )}
         </header>
