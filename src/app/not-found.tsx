94233709ac71598584041009cced54050998c53c
lealdo-laptop <j3leonardodelgado@gmail.com>
2025-04-28 18:20:58 -0400

    fix: update styles in NotFound component for improved visibility and aesthetics

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,16 +12,16 @@ export default function NotFound() {
                 priority
             />
 
-            <h1 className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text p-2 text-5xl font-extrabold text-transparent drop-shadow-lg">
+            <h1 className="bg-gradient-to-r from-purple-600 via-pink-600 to-red-600 bg-clip-text p-2 text-5xl font-extrabold text-transparent drop-shadow-lg">
                 Page Not Found
             </h1>
-            <p className="max-w-md text-lg text-gray-400">
+            <p className="max-w-md text-xl text-gray-300">
                 Oops! The page you are looking for does not exist or has been
                 moved.
             </p>
             <Link
                 href="/"
-                className="mt-4 inline-block rounded-lg bg-gradient-to-r from-purple-700 via-pink-600 to-red-600 px-6 py-3 text-lg font-semibold text-white shadow-lg transition-transform hover:scale-105 hover:from-purple-600 hover:to-red-500 focus:ring-2 focus:ring-purple-400 focus:outline-none"
+                className="mt-4 inline-block rounded-lg bg-gradient-to-r from-purple-700 via-pink-600 to-red-600 px-6 py-3 text-lg font-semibold text-white shadow-lg transition-transform hover:scale-105 hover:from-purple-800 hover:to-red-500 focus:ring-2 focus:ring-purple-400 focus:outline-none"
             >
                 Go back home
             </Link>
