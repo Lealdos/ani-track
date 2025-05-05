a386b995d7ea484ee728004bcee6de30764c52da
lealdo-laptop <j3leonardodelgado@gmail.com>
2025-05-05 13:25:38 -0400

    feat: update BrowseAnime component to handle search parameters and improve pagination logic

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -8,17 +8,27 @@ interface PaginationProps {
 
 export function Pagination(paginationProps: PaginationProps) {
     const { current_Page, has_next_page, last_visible_page } = paginationProps
+    console.log(
+        'current_Page',
+        current_Page,
+        'has_next_page',
+        has_next_page,
+        'last_visible_page',
+        last_visible_page
+    )
 
     return (
         <div className="mt-4 flex flex-row items-center justify-between">
-            <button className="rounded-lg bg-purple-900 px-4 py-2 text-sm font-medium hover:bg-purple-900/90">
-                {current_Page > 1 ? '←' : ''} Previous page
+            <button
+                className={`rounded-lg bg-purple-900 px-4 py-2 text-sm font-medium ${current_Page <= 1 ? 'opacity-50' : 'hover:bg-purple-900/90'}`}
+            >
+                {current_Page >= 1 ? '←' : ''} Previous page
             </button>
             <span className="text-sm font-medium">
                 {current_Page} of {last_visible_page}
             </span>
             <button
-                className="rounded-lg bg-purple-900 px-4 py-2 text-sm font-medium hover:bg-purple-900/90"
+                className={`rounded-lg bg-purple-900 px-4 py-2 text-sm font-medium ${!has_next_page ? 'opacity-50' : 'hover:bg-purple-900/90'}`}
                 disabled={!has_next_page}
             >
                 Next page {has_next_page ? '→' : ''}
