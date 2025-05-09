42448912fe93edfdc4fb1f6e5afbcb46851b7fb5
Jose Delgado <j3leonardodelgado@gmail.com>
2025-05-09 03:53:29 -0400

    refactor: add width class to footer for improved layout consistency

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="flex flex-col items-center justify-center gap-4 border-t bg-gradient-to-bl from-slate-900 via-purple-900 to-slate-900">
+        <footer className="flex w-full flex-col items-center justify-center gap-4 border-t bg-gradient-to-bl from-slate-900 via-purple-900 to-slate-900">
             <div className="flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
                 <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
                     <p className="text-center text-sm leading-loose md:text-left">
