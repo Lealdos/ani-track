3bb657c2cf6b26d68093e52c30e13ad2466e3020
Jose Delgado <j3leonardodelgado@gmail.com>
2025-05-09 03:16:37 -0400

    refactor: enhance SearchBar layout for improved alignment and responsiveness

--- src/components/Header/SearchBar.tsx
+++ src/components/Header/SearchBar.tsx
@@ -21,12 +21,12 @@ export function SearchBar() {
 
     return (
         <form onSubmit={handleSearch} className="w-full">
-            <div className="relative">
+            <div className="relative flex w-full items-center justify-center">
                 <Search className="absolute top-1/2 left-3 h-4 w-4 -translate-y-1/2 text-gray-200" />
                 <input
                     type="search"
                     placeholder="Search animes..."
-                    className="h-10 w-full rounded-lg border border-t-red-700 border-r-red-700 border-b-red-500 border-l-red-500 bg-black/40 p-2 pr-12 pl-10 text-sm text-white placeholder:text-gray-400"
+                    className="h-10 w-full rounded-lg border border-t-red-700 border-r-red-700 border-b-red-500 border-l-red-500 bg-black/40 p-2 pr-10 pl-10 text-sm text-white placeholder:text-gray-400"
                     value={query}
                     onChange={(searchEvent: {
                         target: { value: React.SetStateAction<string> }
