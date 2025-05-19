a8fb577c6695c1dc5b0af7a3dde1d119d6de87dc
Jose Delgado <j3leonardodelgado@gmail.com>
2025-05-18 22:29:55 -0400

    refactor: restructure remotePatterns for image configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
     images: {
-        remotePatterns: [new URL('https://cdn.myanimelist.net/images/**')],
-        domains: ['cdn.myanimelist.net'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.myanimelist.net',
+                pathname: '/images/**',
+            },
+        ],
     },
     experimental: {
         viewTransition: true,
