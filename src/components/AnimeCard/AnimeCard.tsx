e5d19a3acb86f86688522ba50c9a0bbaf0793b68
Jose Delgado <j3leonardodelgado@gmail.com>
2025-06-09 02:24:50 -0400

    refactor: enhance AnimeCard component by adding hasFooter prop and improving layout

--- src/components/AnimeCard/AnimeCard.tsx
+++ src/components/AnimeCard/AnimeCard.tsx
@@ -6,19 +6,27 @@ import Image from 'next/image'
 interface AnimeCardProps {
     anime: Anime
     showBadge?: boolean
+    hasFooter?: boolean
 }
 
-export function AnimeCard({ anime, showBadge = false }: AnimeCardProps) {
+export function AnimeCard({
+    anime,
+    showBadge = false,
+    hasFooter = true,
+}: AnimeCardProps) {
     return (
         <article className="flex h-full flex-col items-center justify-between overflow-hidden rounded-lg border bg-black transition-all duration-400 hover:scale-105 hover:shadow-xl md:w-full">
-            <Link href={`/anime/${anime.mal_id}`} className="w-full">
+            <Link
+                href={`/anime/${anime.mal_id}`}
+                className="flex w-full flex-col"
+            >
                 <div className="relative">
                     <Image
                         src={
                             anime.images?.webp?.image_url || '/placeholder.jpg'
                         }
                         alt={anime.title}
-                        className="h-[360px] max-h-[360px] w-[500px] rounded sm:object-fill md:object-cover"
+                        className="h-[380px] max-h-[380px] w-[500px] rounded sm:object-fill md:object-cover"
                         width={800}
                         height={580}
                         priority
@@ -29,25 +37,21 @@ export function AnimeCard({ anime, showBadge = false }: AnimeCardProps) {
                         </div>
                     )}
                 </div>
-                <section className="p-3">
-                    <h3 className="line-clamp-2 leading-tight font-medium">
-                        {anime.title}
-                    </h3>
-                    <p className="mt-1 text-sm">
-                        {anime.type} •{' '}
-                        {anime.episodes
-                            ? `${anime.episodes} eps`
-                            : 'still airing'}{' '}
-                        •{' '}
-                    </p>
-                    {anime.status}
-                </section>
+                {hasFooter && (
+                    <footer className="p-3">
+                        <h3 className="my-2 line-clamp-2 justify-start leading-tight font-medium">
+                            {anime.title}
+                        </h3>
+                        <p className="mb-2 text-sm">
+                            {anime.type} •{' '}
+                            {anime.episodes
+                                ? `${anime.episodes} eps`
+                                : 'still airing'}{' '}
+                            • {anime.status}
+                        </p>
+                    </footer>
+                )}
             </Link>
-            <div className="pb-3">
-                <button className="bg-primary hover:bg-primary/90 rounded-lg px-4 py-2 text-sm font-medium">
-                    Add to list
-                </button>
-            </div>
         </article>
     )
 }
