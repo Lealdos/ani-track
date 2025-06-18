0b7f46f58b9375dc533f1c4c3cb1e17ca7317535
Jose Delgado <j3leonardodelgado@gmail.com>
2025-06-18 14:04:35 -0400

    refactor: replace img with Image component for better optimization

--- src/components/topAnime/TopAnime.tsx
+++ src/components/topAnime/TopAnime.tsx
@@ -4,6 +4,7 @@ import { AnimeListSkeleton } from '../SkeletonCard/AnimeSkeletonList'
 // import { SkeletonCard } from '../SkeletonCard/skeletonCard'
 import { mergeClassNames } from '@/lib/utils'
 import Link from 'next/link'
+import Image from 'next/image'
 interface TopAnimeProps {
     topAnime: Promise<Anime[]>
 }
@@ -15,20 +16,19 @@ export async function TopAnime({ topAnime }: TopAnimeProps) {
             <Suspense fallback={<AnimeListSkeleton sectionName="top-anime" />}>
                 <main className="flex items-center gap-2 overflow-hidden overflow-x-auto px-2 py-4">
                     {animes?.map((anime) => (
-                        
                         <Link
                             href={`/anime/${anime.mal_id}`}
                             key={anime.mal_id}
-                            className="relative flex flex-row items-center hover:scale-105 transition-transform"
+                            className="relative flex flex-row items-center transition-transform hover:scale-105"
                             aria-label={`top ${anime.rank} - ${anime.title}`}
                         >
                             {/*  ranking number behind the  card */}
                             <span
                                 className={mergeClassNames(
                                     `gradient-top-number pointer-events-none relative text-center font-gothic text-[200px] leading-none text-gray-900 antialiased select-none md:text-[220px]`,
-                                    anime?.rank !== undefined && anime.rank  > 9
-                                    ? 'tracking-[-1.5rem] md:tracking-[-2rem]'
-                                    : ''
+                                    anime?.rank !== undefined && anime.rank > 9
+                                        ? 'tracking-[-1.5rem] md:tracking-[-2rem]'
+                                        : ''
                                 )}
                             >
                                 {anime.rank}
@@ -36,7 +36,7 @@ export async function TopAnime({ topAnime }: TopAnimeProps) {
 
                             {/*  anime card */}
 
-                            <img
+                            <Image
                                 className={mergeClassNames(
                                     `relative h-70 max-w-[200px] min-w-[200px] rounded`,
                                     (anime?.rank ?? 0) > 9
@@ -44,8 +44,10 @@ export async function TopAnime({ topAnime }: TopAnimeProps) {
                                         : '-left-8 md:-left-10'
                                 )}
                                 src={
-                                    anime.images?.webp?.image_url || '/placeholder.jpg'
+                                    anime.images?.webp?.image_url ||
+                                    '/placeholder.jpg'
                                 }
+                                alt={anime.title}
                             />
                         </Link>
                     ))}
