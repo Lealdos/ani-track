c1b40576a4f8c5911b9a90c59eeed0c8e45c4016
j3leonardodelgado <j3leonardodelgado@gmail.com>
2025-06-19 01:09:26 +0000

    fix: margin added

--- src/components/AnimeList/AnimeList.tsx
+++ src/components/AnimeList/AnimeList.tsx
@@ -17,11 +17,11 @@ export function AnimeList({
         animesPromise instanceof Promise ? use(animesPromise) : animesPromise
     if (!animes) return <div>Loading...</div>
     return (
-        <main className="p-2 scrollbar-hide flex snap-x gap-4 overflow-x-auto px-4 pb-4 md:grid md:grid-cols-2 md:gap-4 md:overflow-visible lg:grid-cols-4 xl:grid-cols-4">
+        <main className="scrollbar-hide flex snap-x gap-4 overflow-x-auto px-4 pb-4 md:grid md:grid-cols-2 md:gap-4 md:overflow-visible lg:grid-cols-4 xl:grid-cols-4">
             {animes.map((animeItem) => (
                 <div
                     key={`${animeItem.title}-${animeItem.mal_id}-${SectionName}`}
-                    className="w-[70%] shrink-0 snap-start sm:w-[50%] md:w-[280px]"
+                    className="w-[70%] shrink-0 snap-start sm:w-[50%] md:w-[280px] m-2"
                 >
                     <AnimeCard anime={animeItem} showBadge={showBadge} />
                 </div>
