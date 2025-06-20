e274ecf8a841d9135ee39806c78092f4e1193940
j3leonardodelgado <j3leonardodelgado@gmail.com>
2025-06-20 23:12:52 +0000

    fix: skeleton cards width fixed

--- src/components/SkeletonCard/AnimeSkeletonList.tsx
+++ src/components/SkeletonCard/AnimeSkeletonList.tsx
@@ -10,7 +10,7 @@ export function AnimeListSkeleton({
     skeletonItemCount = 12,
 }: AnimeListSkeletonProps) {
     return (
-        <section className="scrollbar-hide grid snap-x grid-cols-2 justify-items-center gap-4 overflow-x-auto px-4 py-6 md:gap-4 md:overflow-visible lg:grid-cols-3 xl:grid-cols-5">
+        <section className="scrollbar-hide grid snap-x grid-cols-2 justify-items-center gap-4 overflow-x-auto px-4 py-6 sm:grid-cols-3 md:gap-4 md:overflow-visible lg:grid-cols-4 xl:grid-cols-5">
             {[...Array(skeletonItemCount)].map((_, i) => (
                 <SkeletonCard key={`${sectionName}-${i}`} />
             ))}
--- src/components/SkeletonCard/skeletonCard.tsx
+++ src/components/SkeletonCard/skeletonCard.tsx
@@ -1,6 +1,6 @@
 export function SkeletonCard() {
     return (
-        <article className="h-80  md:h-full max-w-[200px] md:max-w-[320px] flex animate-pulse flex-col items-center justify-between overflow-hidden rounded-lg border bg-slate-800 max-h-[420px]">
+        <article className="h-80 w-full md:h-full max-w-[200px] md:max-w-[320px] flex animate-pulse flex-col items-center justify-between overflow-hidden rounded-lg border bg-slate-800 max-h-[420px]">
             <div className="bg-muted relative aspect-[1/3] w-full rounded" />
             <section className="w-full p-3">
                 <div className="bg-muted mb-2 h-4 w-3/4 rounded" />
