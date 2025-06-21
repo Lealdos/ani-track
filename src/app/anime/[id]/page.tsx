6effd3bc80645810617ee27be0c204c44bc35074
j3leonardodelgado <j3leonardodelgado@gmail.com>
2025-06-21 02:51:01 +0000

    refactor:  make anime recomendations title bigger

--- src/app/anime/[id]/page.tsx
+++ src/app/anime/[id]/page.tsx
@@ -276,8 +276,8 @@ export default async function AnimePage({
                                                 height={300}
                                                 className="aspect-[2/3] w-full object-cover"
                                             />
-                                            <div className="absolute right-0 bottom-0 left-0 bg-gradient-to-t from-black/80 to-transparent p-3">
-                                                <h3 className="line-clamp-2 text-sm font-medium">
+                                            <div className="absolute right-0 bottom-0 left-0 bg-gradient-to-t from-black to-transparent p-2">
+                                                <h3 className="line-clamp-2 text-lg font-medium text-balance text-center">
                                                     {
                                                         recommendedAnime.entry
                                                             .title
