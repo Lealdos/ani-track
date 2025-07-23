718c23f335533ccecf58937a606baf6bcd3e7520
Jose Delgado <j3leonardodelgado@gmail.com>
2025-07-23 16:49:11 -0400

    feat: implement FavoritesAccordion component with favorite animes management

--- src/components/ui/FavoritesAccordion.tsx
+++ src/components/ui/FavoritesAccordion.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { useState } from 'react' 
-import { getStoredFavoriteAnimes } from '@/store/userFavoritesAnime'
+import { useState } from 'react'
+// import { getStoredFavoriteAnimes } from '@/store/StorageFavoritesAnime'
 import {
     Accordion,
     AccordionContent,
@@ -10,46 +10,21 @@ import {
 } from '@/components/ui/accordion'
 import { SkeletonCard } from '@/components/SkeletonCard/skeletonCard'
 
-// import { getAnimeByGenre } from '@/lib/api'
 import { Anime } from '@/types/anime'
 import { AnimeCard } from '../AnimeCard/AnimeCard'
-
-
+import { useFavoriteAnimes } from '@/hooks/useFetchUserFavoriteList'
 
 // remember to add Types
 
-
 export function FavoritesAccordion() {
-    const [favoriteLists] = useState<Anime[]>(() => getStoredFavoriteAnimes() )
-    const [loading] = useState(false)
-    const [error] = useState<string | null>(null)
-
-    // useEffect(() => {
-    //     const loadListData = async () => {
-    //         try {
-    //             const romanceAnime = await getAnimeByGenre(22)
-    //             updateListData( romanceAnime)
-    //             setLoading(false)
+    const { favorites, removeFromFavorites } = useFavoriteAnimes()
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState<string | null>(null)
 
     //             // const currentYear = new Date().getFullYear()
     //             // const seasons = ["winter", "spring", "summer", "fall"]
     //             // const currentMonth = new Date().getMonth()
     //             // const currentSeason = seasons[Math.floor(currentMonth / 3)]
-
-            
-    //         } catch (error) {
-    //             setError(`Error loading data error ${error}`)
-    //             console.error('Error loading data:', error)
-    //         }
-    //     }
-
-    //     loadListData()
-    // }, [])
-
-    // const updateListData = ( FavAnimes: Anime[]) => {
-    //     setFavoriteLists(FavAnimes)
-
-    // }
 
     return (
         <div className="mx-auto w-full py-2">
@@ -59,43 +34,40 @@ export function FavoritesAccordion() {
                 className="w-full overflow-hidden rounded-lg border"
             >
                 <AccordionItem value="favorite-list" className="border-0">
-                    <AccordionTrigger className="hover:bg-muted/50 px-4 py-3 transition-all text-sm">
+                    <AccordionTrigger className="hover:bg-muted/50 px-4 py-3 text-sm transition-all">
                         <div className="flex items-center gap-2">
-                            <span className="rounded-full px-2 py-0.5  font-medium dark:bg-purple-900/20 ">
-                                Animes in this list: {favoriteLists.length}
+                            <span className="rounded-full px-2 py-0.5 font-medium dark:bg-purple-900/20">
+                                Animes in this list: {favorites.length}
                             </span>
                         </div>
                     </AccordionTrigger>
                     <AccordionContent className="max-h-[720px] overflow-auto px-4 pt-2 pb-4">
                         <div className="space-y-4">
-                        {loading ? (
-                        <div className="grid md:grid gap-4 sm:grid-cols-2 md:grid-cols-4">
-                            {[...Array(6)].map((_, index) => (
-                                <SkeletonCard key={index} />
-                            ))}
-                        </div>
-                    ) : error ? (
-                        <div className="p-4 text-center text-red-500">
-                            <p>Error: {error}</p>
-                        </div>
-                    ) : favoriteLists.length > 0 ? (
-                        <div className="grid md:grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center
-                        ">
-                             {favoriteLists.map((anime) => (
-                                <AnimeCard
-                                    showBadge
-                                    key={`${anime.mal_id}-${anime.title}`}
-                                    anime={anime}
-                                />
-                            ))}
-                        </div>
-                    ) : (
-                        <span className="block px-2 py-0.5 text-lg font-medium text-slate-100 text-center">
-                            No animes in the list
-                        </span>
-                    )}
-
-                          
+                            {loading ? (
+                                <div className="grid gap-4 sm:grid-cols-2 md:grid md:grid-cols-4">
+                                    {[...Array(6)].map((_, index) => (
+                                        <SkeletonCard key={index} />
+                                    ))}
+                                </div>
+                            ) : error ? (
+                                <div className="p-4 text-center text-red-500">
+                                    <p>Error: {error}</p>
+                                </div>
+                            ) : favorites.length > 0 ? (
+                                <div className="grid justify-items-center gap-4 sm:grid-cols-2 md:grid md:grid-cols-3 lg:grid-cols-4">
+                                    {favorites.map((anime: Anime) => (
+                                        <AnimeCard
+                                            showBadge
+                                            key={`${anime.mal_id}-${anime.title}-Favorites`}
+                                            anime={anime}
+                                        />
+                                    ))}
+                                </div>
+                            ) : (
+                                <span className="block px-2 py-0.5 text-center text-lg font-medium text-slate-100">
+                                    No animes in the list
+                                </span>
+                            )}
                         </div>
                     </AccordionContent>
                 </AccordionItem>
