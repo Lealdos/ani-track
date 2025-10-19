410dfb3d74ca79ebc88c2155f4eabefedd44a217
j3leonardodelgado <j3leonardodelgado@gmail.com>
2025-10-19 20:21:11 +0000

    feature:  Speed Insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import dynamic from 'next/dynamic'
 import { Inter, Roboto_Mono, Dela_Gothic_One } from 'next/font/google'
 import './globals.css'
@@ -39,16 +40,18 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
+        
         <ClerkProvider
-            appearance={{
-                baseTheme: dark,
-            }}
-            afterSignOutUrl="/"
+        appearance={{
+            baseTheme: dark,
+        }}
+        afterSignOutUrl="/"
         >
             <html lang="en">
                 <body
                     className={`${inter.variable} ${roboto_mono.variable} ${delaGothicOne.variable} flex w-full flex-col items-center-safe justify-between scroll-smooth bg-gray-950 font-sans text-white antialiased`}
-                >
+                    >
+                        <SpeedInsights/>
                     <Header />
 
                     <ViewTransition> {children}</ViewTransition>
