5054cac020b8e4cee49eed768e9faaf21725f8f3
Jose Delgado <j3leonardodelgado@gmail.com>
2025-10-30 23:48:08 -0400

    fix: reduce gap in AnimeList component for improved layout

--- src/components/AnimeList/AnimeList.tsx
+++ src/components/AnimeList/AnimeList.tsx
@@ -17,7 +17,7 @@ export function AnimeList({
         animesPromise instanceof Promise ? use(animesPromise) : animesPromise
     if (!animes) return <div>Loading...</div>
     return (
-        <ul className="grid grid-cols-2 justify-items-center gap-4 px-4 py-4 sm:grid-cols-3 md:overflow-visible lg:grid-cols-4 xl:grid-cols-5">
+        <ul className="grid grid-cols-2 justify-items-center gap-2 px-4 py-4 sm:grid-cols-3 md:overflow-visible lg:grid-cols-4 xl:grid-cols-5">
             {animes.map((animeItem) => (
                 <AnimeCard
                     key={`${animeItem.title}-${animeItem.mal_id}-${SectionName}`}
