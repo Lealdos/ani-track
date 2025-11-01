dc2361d54abbb50eb5f97cfc2fb8c19cbc5248ec
j3leonardodelgado <j3leonardodelgado@gmail.com>
2025-11-01 03:29:31 +0000

    fix: remove border from anime card skeleton

--- src/shared/components/SkeletonCard/skeletonCard.tsx
+++ src/shared/components/SkeletonCard/skeletonCard.tsx
@@ -1,6 +1,6 @@
 export function SkeletonCard() {
     return (
-        <article className="flex h-80 max-h-[360px] w-full max-w-[200px] animate-pulse flex-col items-center justify-between overflow-hidden rounded-lg border bg-slate-800 md:h-full md:max-w-[240px]">
+        <article className="flex h-80 max-h-[360px] w-full max-w-[200px] animate-pulse flex-col items-center justify-between overflow-hidden rounded-lg bg-slate-800 md:h-full md:max-w-[200px]">
             <div className="bg-muted relative aspect-[1/3] w-full rounded" />
             <section className="w-full p-3">
                 <div className="bg-muted mb-2 h-4 w-3/4 rounded" />
