98cbcc08667596b9a35ee5adc32ab32639e6ae35
j3leonardodelgado <j3leonardodelgado@gmail.com>
2025-11-01 03:35:05 +0000

    fix: adjust img size

--- src/app/(home)/components/EpisodeSchedule/EpisodeSchedule.tsx
+++ src/app/(home)/components/EpisodeSchedule/EpisodeSchedule.tsx
@@ -90,8 +90,9 @@ export function EpisodeSchedule(): React.ReactElement {
                                         anime.images?.jpg?.image_url ||
                                         '/placeholder.svg'
                                     }
+                                     
                                     alt={`${anime.title} poster`}
-                                    className="h-50 max-h-80 rounded md:h-72 md:w-full md:object-center"
+                                    className="h-50 max-h-90 rounded md:h-72 md:w-full md:object-center object-fit "
                                 />
                                 <span className="absolute top-3 right-3 rounded-full bg-black/70 px-2 py-1 text-xs text-white">
                                     {convertJSTToLocal(anime.broadcast?.string)}
