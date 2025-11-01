fed5d58cf3bd9d41a24f600117a4574b68395c2e
j3leonardodelgado <j3leonardodelgado@gmail.com>
2025-11-01 01:09:49 +0000

    feat: create loading component for pages

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { Loader } from '@/shered/components/loading/Loader';
+
+export default function Loading() {
+    return (
+        <div className='w-full h-full my-56'>
+            <Loader/>
+        </div>
+    );
+}
