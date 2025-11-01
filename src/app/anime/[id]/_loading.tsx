fd01eba88aec8512344d76936147b1eacc0f5f02
j3leonardodelgado <j3leonardodelgado@gmail.com>
2025-11-01 01:22:38 +0000

    remove: loading component from bundler

---
+++ src/app/anime/[id]/_loading.tsx
@@ -0,0 +1,9 @@
+import { Loader } from '@/shared/components/loading/Loader';
+
+export default function Loading() {
+    return (
+        <div className='w-full h-full my-56'>
+            <Loader/>
+        </div>
+    );
+}
