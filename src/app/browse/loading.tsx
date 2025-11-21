cb03f7627176bfe757f44ef11f7dc9e1b1914dc0
j3leonardodelgado <j3leonardodelgado@gmail.com>
2025-11-21 14:34:05 +0000

    feature: - add loading page to for browser/anime page

---
+++ src/app/browse/loading.tsx
@@ -0,0 +1,9 @@
+import { Loader } from '@/shared/components/loading/Loader'
+
+export default function Loading() {
+    return (
+        <div className="my-56 h-svh w-full">
+            <Loader />
+        </div>
+    )
+}
