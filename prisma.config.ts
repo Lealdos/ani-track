6ad3014d66100509dd9420bd702e19c9b3c51746
j3leonardodelgado <j3leonardodelgado@gmail.com>
2025-11-21 17:32:43 +0000

    refactor: create new prisma config file

---
+++ prisma.config.ts
@@ -0,0 +1,23 @@
+
+import { defineConfig } from 'prisma/config';
+
+
+type Env = 'local' | 'staging' | 'production'
+
+const env = (process.env.NODE_ENV as Env) ?? 'local'
+
+const urlByEnv: Record<Env, string> = {
+  local: process.env.DATABASE_URL_LOCAL!,
+  staging: process.env.DATABASE_URL_STG!,
+  production: process.env.DATABASE_URL_PROD!,
+}
+
+export default defineConfig({
+    schema: 'prisma/schema.prisma',
+    migrations: {
+      path: 'prisma/migrations',
+    },
+    datasource: {
+      url: urlByEnv[env],
+    },
+  });
