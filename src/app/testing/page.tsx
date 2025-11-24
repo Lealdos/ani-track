1a25febeac9aa5ee1ab46f926dbad3921fcb9248
Jose Delgado <j3leonardodelgado@gmail.com>
2025-11-23 20:38:25 -0500

    feat: remove loading component from testing page

--- src/app/testing/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader } from '@/shared/components/loading/Loader';
-
-export default function Loading() {
-    return (
-        <div className='w-full h-full my-56'>
-            <Loader/>
-        </div>
-    );
-}
