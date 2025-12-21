a9c9b29b00997c63752d91eb7b570c26c7aba5d8
Jose Delgado <j3leonardodelgado@gmail.com>
2025-12-21 13:20:38 -0500

    feat: add loading component for authentication flow

---
+++ src/app/(auth)/loading.tsx
@@ -0,0 +1,9 @@
+import { Loader } from '@/components/shared/loading/Loader'
+
+export default function Loading() {
+    return (
+        <div className="my-56 h-svh w-full">
+            <Loader />
+        </div>
+    )
+}
