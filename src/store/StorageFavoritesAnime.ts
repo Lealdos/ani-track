74991d08e4ffb8f240cc2d5540135845be0bf2db
Jose Leonardo Delgado Avila <j3leonardodelgado@gmail.com>
2026-04-28 17:41:59 -0400

    refactor: update favorite anime storage functions to use Anime model

--- src/store/StorageFavoritesAnime.ts
+++ src/store/StorageFavoritesAnime.ts
@@ -1,15 +1,15 @@
 'use client'
-import { JikanAnime } from '@/services/JikanAPI/interfaces/JikanType'
+import type { Anime } from '@/entities/anime/models'
 
 const FAV_KEY = 'Favorites-Animes'
 
-export function getStoredFavoriteAnimes() {
+export function getStoredFavoriteAnimes(): Anime[] {
     if (globalThis.window === undefined) return []
     const FavoriteAnimeList = localStorage.getItem(FAV_KEY)
     return FavoriteAnimeList ? JSON.parse(FavoriteAnimeList) : []
 }
 
-export function storeFavoriteAnimes(favorites: JikanAnime[]) {
+export function storeFavoriteAnimes(favorites: Anime[]) {
     if (globalThis.window === undefined) return
     localStorage.setItem(FAV_KEY, JSON.stringify(favorites))
 }
