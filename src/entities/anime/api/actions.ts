5dfc5b6ac2ff6689f0d84133b6b363c5b65cb91e
Jose Leonardo Delgado Avila <j3leonardodelgado@gmail.com>
2026-05-03 06:26:24 -0400

    feat: update getAiringByDayAction to use fetchAiringByDay for improved data retrieval

--- src/entities/anime/api/actions.ts
+++ src/entities/anime/api/actions.ts
@@ -1,11 +1,14 @@
 'use server'
 import { animeRepository } from './JikanAnimeRepository'
+import { fetchAiringByDay, type AiringAnime } from './anilistSchedule'
 import type { ScheduleDay } from '../models'
 
 export async function getAnimeByGenreAction(genreId: number) {
     return animeRepository.findByGenre(genreId)
 }
 
-export async function getAiringByDayAction(day: ScheduleDay) {
-    return animeRepository.findAiringByDay(day)
+export async function getAiringByDayAction(
+    day: ScheduleDay
+): Promise<AiringAnime[]> {
+    return fetchAiringByDay(day)
 }
