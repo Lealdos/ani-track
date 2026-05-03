cf18c2c80c4683726ccaecde450dd45fb59cf7fb
Jose Leonardo Delgado Avila <j3leonardodelgado@gmail.com>
2026-05-03 06:26:35 -0400

    feat: refactor TopAnime component to use list for better semantic structure

--- src/app/(home)/components/topAnime/TopAnime.tsx
+++ src/app/(home)/components/topAnime/TopAnime.tsx
@@ -11,45 +11,46 @@ export async function TopAnime({ topAnime }: TopAnimeProps) {
     const animes = await topAnime
     return (
         <section className="m-4">
-            <main className="flex items-center gap-2 overflow-hidden overflow-x-auto px-2 py-4">
+            <ul className="flex items-center gap-2 overflow-hidden overflow-x-auto px-2 py-4">
                 {animes?.map((anime) => (
-                    <Link
-                        href={`/anime/${anime.id}`}
-                        key={anime.id}
-                        className="relative flex flex-row items-center transition-transform hover:scale-105"
-                        aria-label={`top ${anime.rank} - ${anime.title}`}
-                    >
-                        {/*  ranking number behind the  card */}
-                        <span
-                            className={cn(
-                                `gradient-top-number pointer-events-none relative text-center font-gothic text-[200px] leading-none text-gray-900 antialiased select-none md:text-[220px]`,
-                                anime?.rank !== undefined && anime.rank > 9
-                                    ? 'tracking-[-1.5rem] md:tracking-[-2rem]'
-                                    : ''
-                            )}
+                    <li key={`${anime.id}-top-animes`}>
+                        <Link
+                            href={`/anime/${anime.id}`}
+                            className="relative flex flex-row items-center transition-transform hover:scale-105"
+                            aria-label={`top ${anime.rank} - ${anime.title}`}
                         >
-                            {anime.rank}
-                        </span>
+                            {/*  ranking number behind the  card */}
+                            <span
+                                className={cn(
+                                    `gradient-top-number pointer-events-none relative text-center font-gothic text-[200px] leading-none text-gray-900 antialiased select-none md:text-[220px]`,
+                                    anime?.rank !== undefined && anime.rank > 9
+                                        ? 'tracking-[-1.5rem] md:tracking-[-2rem]'
+                                        : ''
+                                )}
+                            >
+                                {anime.rank}
+                            </span>
 
-                        {/*  anime card */}
-                        <img
-                            className={cn(
-                                `relative h-70 max-w-[200px] min-w-[200px] rounded`,
-                                (anime?.rank ?? 0) > 9
-                                    ? '-left-6 md:-left-12'
-                                    : '-left-8 md:-left-10'
-                            )}
-                            width={800}
-                            height={580}
-                            src={
-                                anime.images?.webp?.imageUrl ||
-                                '/placeholder.svg'
-                            }
-                            alt={anime.title}
-                        />
-                    </Link>
+                            {/*  anime card */}
+                            <img
+                                className={cn(
+                                    `relative h-70 max-w-[200px] min-w-[200px] rounded`,
+                                    (anime?.rank ?? 0) > 9
+                                        ? '-left-6 md:-left-12'
+                                        : '-left-8 md:-left-10'
+                                )}
+                                width={800}
+                                height={580}
+                                src={
+                                    anime.images?.webp?.imageUrl ||
+                                    '/placeholder.svg'
+                                }
+                                alt={anime.title}
+                            />
+                        </Link>
+                    </li>
                 ))}
-            </main>
+            </ul>
         </section>
     )
 }
